0df44b66adcf65af9fe867acd8055af763d4f29f
zzeen2 <jking120393@gmail.com>
2025-06-05 16:46:43 +0900

    kakao_login_app

--- server.js
+++ server.js
@@ -46,6 +46,66 @@ app.use('/api/setting', SettingsRouter);
 app.use('/list', ListRouter)
 app.use('/stats', StatsRouter);
 
+­// GET 요청 처리 엔드포인트 추가
+app.get("/oauth", async (req, res) => {
+  const { code } = req.query;
+  
+  if (!code) {
+    return res.status(400).json({
+      result: 'fail',
+      error: "인가 코드가 필요합니다."
+    });
+  }
+
+  try {
+    // 카카오 토큰 요청
+    const tokenResponse = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      {
+        grant_type: "authorization_code",
+        client_id: process.env.KAKAO_CLIENT_ID,
+        redirect_uri: process.env.KAKAO_REDIRECT_URI,
+        code: code,
+        client_secret: process.env.CLIENT_SECRET,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+        }
+      }
+    );
+
+    const { access_token } = tokenResponse.data;
+
+    // 성공 응답
+    res.redirect(`https://devops1.store/?access_token=${access_token}`);
+
+  } catch (error) {
+    console.error("카카오 로그인 처리 중 오류:", error.response?.data || error.message);
+    res.status(500).send();
+  }
+});
+
+// 카카오 사용자 정보 요청
+app.get("/", async (req, res) => {
+    const { access_token } = req.query;
+    // 카카오 사용자 정보 요청
+    const userResponse = await axios.get(
+        "https://kapi.kakao.com/v2/user/me",
+        {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+            }
+        }
+    );
+    
+    const { id, properties } = userResponse.data;
+    const { nickname } = properties;
+    res.json({id, properties, nickname});
+
+});
+
 app.listen(4000, (req,res)=> {
     console.log("server on")
 })
